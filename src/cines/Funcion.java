a846971e7e25eff32b69eb20c508abd42c44cd8a
edjavier <>
2017-05-26 19:10:26 -0300

    Incluidos los atributos de relacion entre clases

--- src/cines/Funcion.java
+++ src/cines/Funcion.java
@@ -1,6 +1,7 @@
 package cines;
 
 import java.util.Date;
+import java.util.List;
 
 public class Funcion {
     
@@ -10,15 +11,21 @@ public class Funcion {
     private int duracion;
     private Date horaInicio;
     private int numero;
+    private Pelicula pelicula;
+    private Sala sala;
+    private List<Entrada> entradas;
     
     //metodo constructor (reserva memoria e inicializa las variables miembros
     //de la clase)
     
-    public Funcion(int diaSemana, int duracion, Date horaInicio, int numero){
+    public Funcion(int diaSemana, int duracion, Date horaInicio, int numero, Pelicula pelicula, Sala sala, List<Entrada> entradas){
         this.diaSemana = diaSemana;
         this.duracion = duracion;
         this.horaInicio = horaInicio;
         this.numero = numero;
+        this.pelicula = pelicula;
+        this.sala = sala;
+        this.entradas = entradas;
     }
     
     //metodos de seteo
@@ -53,5 +60,29 @@ public class Funcion {
     
     public void setNumero(int numero){
         this.numero = numero;
+    }
+    
+    public Pelicula getPelicula(){
+        return this.pelicula;
+    }
+    
+    public void setPeliculas (Pelicula pelicula){
+        this.pelicula = pelicula;
+    }
+    
+    public Sala getSala(){
+        return this.sala;
+    }
+    
+    public void setSala(Sala sala){
+        this.sala = sala;
+    }
+    
+    public List<Entrada> getEntradas(){
+        return this.entradas;
+    }
+    
+    public void setEntradas(List<Entrada> entradas){
+        this.entradas = entradas;
     }
 }
